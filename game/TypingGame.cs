78644b74026c2b4b3106ca31b607ffce495cd41e
MotorGorilla <wilddude453@gmail.com>
2024-04-29 22:58:09 -0400

    TypingGame Documentation

--- game/TypingGame.cs
+++ game/TypingGame.cs
@@ -34,6 +34,10 @@ namespace Multitasking
         private const int lineSpacing = 60;
         private const int verticalOffset = 150;
 
+        /// <summary>
+        /// returns whether the player is currently typing the letter,
+        /// prompt one, prompt two, or prompt three.
+        /// </summary>
         private List<String> CurrentPrompt
         {
             get
@@ -49,6 +53,11 @@ namespace Multitasking
             }
         }
 
+        /// <summary>
+        /// Sole constructor for the typing simulator. 
+        /// </summary>
+        /// <param name="window">the space in the current window that the typing simulator will be displayed</param>
+        /// <param name="day">the current day of gameplay</param>
         public TypingGame(Rectangle window, int day)
         {
             // initializing variables
@@ -66,7 +75,7 @@ namespace Multitasking
                                                             { "s", Keys.S }, { "t", Keys.T }, { "u", Keys.U }, { "v", Keys.V }, { "w", Keys.W }, { "x", Keys.X },
                                                             { "y", Keys.Y }, { "z", Keys.Z }};
             typingWindow = new Rectangle(window.X, window.Y, window.Width, window.Height);
-            currentPromptIndex = -1;
+            currentPromptIndex = -1; // sets the current prompt to the letter
             currentLineIndex = 0;
             currentCharIndex = 0;
             this.day = day;
@@ -79,71 +88,100 @@ namespace Multitasking
 
         // CORE GAME METHODS
 
+        /// <summary>
+        /// frame by frame updates to the typing simulator. mainly checks
+        /// if the user is pressing the right keys or not, and progresses
+        /// the current prompt accordingly.
+        /// </summary>
+        /// <param name="currentState">the current game state</param>
+        /// <returns>the new (or same) game state</returns>
         public GameState UpdateDayLetter(GameState currentState)
         {
             KeyboardState keyboardState = Keyboard.GetState();
             this.currentState = currentState;
+
+            // conditions to make sure the player is typing the right characters and not abusing the system
             bool endOfPrompt;
             bool exceededKeysPressed;
             bool pressingCurrentKey;
             bool changedKeyboardState;
 
+            // checking if the prompt has ended
             if(currentLineIndex == -1)
             {
+                // if it has, move on to the first task prompt and reset the other variables accordingly
                 currentPromptIndex = 0;
                 currentLineIndex = 0;
                 currentCharIndex = 0;
                 previousKeyBoardState = keyboardState;
-                return GameState.Day;
+                return GameState.Day; // makes sure Game1 knows that we are no longer in the letter state
             }
+            // this means we are in the middle of the prompt, not the end
             else
             {
+                // updates conditionals to ensure player is typing the right character and not abusing the simulator
                 endOfPrompt = currentLineIndex != -1;
                 exceededKeysPressed = GetPressedLetterCount(keyboardState) <= 2;
                 pressingCurrentKey = keyboardState.IsKeyDown(GetKeyFromChar(letterPrompt[currentLineIndex].Substring(currentCharIndex, 1)));
                 changedKeyboardState = previousKeyBoardState != keyboardState;
 
+                // checks if they pass every conditional
                 if(endOfPrompt && exceededKeysPressed && pressingCurrentKey && changedKeyboardState)
                 {
+                    // "type" the current character
                     TypeChar();
                 }
 
                 previousKeyBoardState = keyboardState;
-                return GameState.DayLetter;
+                return GameState.DayLetter; // makes sure Game1 knows we are still in the letter state, the day has NOT started
             }   
         }
 
+        /// <summary>
+        /// frame by frame updates to the task prompts, essentially
+        /// the actual "non tutorial" gameplay.
+        /// </summary>
         public void UpdateDay()
         {
-            Debug.WriteLine("currentPromptIndex: " + currentPromptIndex);
             KeyboardState keyboardState = Keyboard.GetState();
+
+            // conditions to make sure the player is typing the right characters and not abusing the system
             bool endOfPrompt;
             bool exceededKeysPressed;
             bool pressingCurrentKey;
             bool changedKeyboardState;
 
+            // checking if the prompt has ended (unfortunately hard coded)
             if(currentLineIndex == -1)
             {
+                // if it has, check if this is the first task prompt
                 if(currentPromptIndex == 0)
                 {
+                    // if it is, move to the second task prompt
                     currentPromptIndex = 1;
                 }
+                // otherwise check if this is the second task prompt and that the day has more than just 2 prompts
                 else if(currentPromptIndex == 1 && day > 5)
                 {
+                    // if it is, moves to the third task prompt
                     currentPromptIndex = 2;
                 }
                 currentLineIndex = 0;
                 currentCharIndex = 0;
             }
+            // this means we are in the middle of the prompt, not the end
             else
             {
+                // updates conditionals to ensure player is typing the right character and not abusing the simulator
                 endOfPrompt = currentLineIndex != -1;
                 exceededKeysPressed = GetPressedLetterCount(keyboardState) <= 2;
                 pressingCurrentKey = keyboardState.IsKeyDown(GetKeyFromChar(CurrentPrompt[currentLineIndex].Substring(currentCharIndex, 1)));
                 changedKeyboardState = previousKeyBoardState != keyboardState;
 
+                // checks if they pass every conditional
                 if(endOfPrompt && exceededKeysPressed && pressingCurrentKey && changedKeyboardState)
                 {
+                    // "type" the current character
                     TypeChar();
                 }
             }
@@ -151,35 +189,36 @@ namespace Multitasking
             previousKeyBoardState = keyboardState;
         }
 
+        /// <summary>
+        /// draws the letter prompt frame by frame.
+        /// </summary>
+        /// <param name="spriteBatch">the SpriteBatch the method is going to draw with</param>
+        /// <param name="font">the font the method is going to use to draw</param>
+        /// <param name="fontBold">the bold version of the font that the method is going to use to draw</param>
+        /// <param name="whitePixel">an image of a white pixel, which is used to draw some things that ShapeBatch can't</param>
+        /// <param name="screenWidth">the width of the entire screen</param>
+        /// <param name="screenHeight">the height of the entire screen</param>
         public void DrawDayLetter(SpriteBatch spriteBatch, SpriteFont font, SpriteFont fontBold, Texture2D whitePixel, int screenWidth, int screenHeight)
         {
+            // goes through each line in the letter prompt
             for(int i = 0; i < letterPrompt.Count; i++)
             {
-
+                // if the line has been typed
                 if(i < currentLineIndex)
                 {
+                    // draw the line as yellow text
                     spriteBatch.DrawString(font, letterPrompt[i], new Vector2((screenWidth / 2) - 400, lineSpacing * i + verticalOffset), Color.Yellow);
                 }
+                // if the line is still to come
                 else if(i > currentLineIndex)
                 {
-                    letterPrompt[i] = letterPrompt[i].TrimEnd();
-                    bool flag = true;
-
-                    for(int j = 0; j < letterPrompt[i].Length; j++)
-                    {
-                        if(!font.Characters.Contains(letterPrompt[i][j]))
-                        {
-                            flag = false;
-                        }
-                    }
-
-                    if(flag)
-                    { 
-                        spriteBatch.DrawString(font, letterPrompt[i], new Vector2((screenWidth / 2) - 400, lineSpacing * i + verticalOffset), Color.Gray);
-                    }
+                    // draws the line as gray text
+                    spriteBatch.DrawString(font, letterPrompt[i], new Vector2((screenWidth / 2) - 400, lineSpacing * i + verticalOffset), Color.Gray);
                 }
+                // this means we are on this line
                 else
                 {
+                    // draws untyped text on this line as blue, typed text as white, a highlight for the current line, and another box on top to ensure the text is clean
                     spriteBatch.DrawString(font, letterPrompt[i].Substring(0, letterPrompt[i].Length), new Vector2((screenWidth / 2) - 400, lineSpacing * i + verticalOffset), new Color(31, 0, 171));
                     spriteBatch.DrawString(fontBold, letterPrompt[i].Substring(0, currentCharIndex), new Vector2((screenWidth / 2) - 400, lineSpacing * i + verticalOffset), Color.White);
                     spriteBatch.Draw(whitePixel, new Rectangle(0, lineSpacing * i + verticalOffset, (int)font.MeasureString(letterPrompt[i].Substring(0, currentCharIndex)).X + 550, 35), Color.White);
@@ -189,34 +228,37 @@ namespace Multitasking
             }
         }
 
+        /// <summary>
+        /// draws the task prompts frame by frame. uses the
+        /// typing window parameters provided when the typing
+        /// simulator was initialized, rather than the full
+        /// screen.
+        /// </summary>
+        /// <param name="spriteBatch">the SpriteBatch the method is going to draw with</param>
+        /// <param name="font">the font the method is going to use to draw</param>
+        /// <param name="fontBold">the bold version of the font that the method is going to use to draw</param>
+        /// <param name="whitePixel">an image of a white pixel, which is used to draw some things that ShapeBatch can't</param>
         public void DrawDay(SpriteBatch spriteBatch, SpriteFont font, SpriteFont fontBold, Texture2D whitePixel)
         {
+            // goes through each line in the letter prompt
             for(int i = 0; i < CurrentPrompt.Count; i++)
             {
+                // if the line has been typed
                 if(i < currentLineIndex)
                 {
+                    // draw the line as yellow text
                     spriteBatch.DrawString(font, CurrentPrompt[i], new Vector2((typingWindow.Width / 2) - 400, lineSpacing * i + verticalOffset), Color.Yellow);
                 }
+                // if the line is still to come
                 else if(i > currentLineIndex)
                 {
-                    CurrentPrompt[i] = CurrentPrompt[i].TrimEnd();
-                    bool flag = true;
-
-                    for(int j = 0; j < CurrentPrompt[i].Length; j++)
-                    {
-                        if(!font.Characters.Contains(CurrentPrompt[i][j]))
-                        {
-                            flag = false;
-                        }
-                    }
-
-                    if(flag)
-                    {
-                        spriteBatch.DrawString(font, CurrentPrompt[i], new Vector2((typingWindow.Width / 2) - 400, lineSpacing * i + verticalOffset), Color.Gray);
-                    }
+                    // draws the line as gray text
+                    spriteBatch.DrawString(font, CurrentPrompt[i], new Vector2((typingWindow.Width / 2) - 400, lineSpacing * i + verticalOffset), Color.Gray);
                 }
+                // this means we are on this line
                 else
                 {
+                    // draws untyped text on this line as blue, typed text as white, a highlight for the current line, and another box on top to ensure the text is clean
                     spriteBatch.DrawString(font, CurrentPrompt[i].Substring(0, CurrentPrompt[i].Length), new Vector2((typingWindow.Width / 2) - 400, lineSpacing * i + verticalOffset), new Color(31, 0, 171));
                     spriteBatch.DrawString(fontBold, CurrentPrompt[i].Substring(0, currentCharIndex), new Vector2((typingWindow.Width / 2) - 400, lineSpacing * i + verticalOffset), Color.White);
                     spriteBatch.Draw(whitePixel, new Rectangle(0, lineSpacing * i + verticalOffset, (int)font.MeasureString(CurrentPrompt[i].Substring(0, currentCharIndex)).X + 75, 35), Color.White);
@@ -232,18 +274,27 @@ namespace Multitasking
 
         // UTILITY METHODS
 
+        /// <summary>
+        /// scrapes a text file and puts the prompts contained
+        /// inside of it into a given list.
+        /// </summary>
+        /// <param name="filepath">the path to the text file</param>
+        /// <param name="day">the current day</param>
         public void ScrapeFile(String filepath, int day)
         {
+            // starts the StreamReader
             StreamReader reader = new StreamReader(filepath);
             reader.ReadLine();
             String currentScrapedLine = reader.ReadLine();
 
+            // scrapes the letter
             while(!currentScrapedLine.Contains("##"))
             {
                 letterPrompt.Add(currentScrapedLine);
                 currentScrapedLine = reader.ReadLine();
             }
 
+            // scrapes task one
             currentScrapedLine = reader.ReadLine();
             while(!currentScrapedLine.Contains("##"))
             {
@@ -251,6 +302,7 @@ namespace Multitasking
                 currentScrapedLine = reader.ReadLine();
             }
 
+            // scrapes task two
             currentScrapedLine = reader.ReadLine();
             while(!currentScrapedLine.Contains("##"))
             {
@@ -258,6 +310,7 @@ namespace Multitasking
                 currentScrapedLine = reader.ReadLine();
             }
 
+            // scrapes task three (if it exists)
             if(day > 5)
             {
                 reader.ReadLine();
@@ -271,20 +324,36 @@ namespace Multitasking
             reader.Close();
         }
 
+        /// <summary>
+        /// gets the number of letters on the player's
+        /// keyboard that was pressed this frame.
+        /// </summary>
+        /// <param name="keyboardState">the player's current KeyboardState</param>
+        /// <returns>the number of letters the player pressed this frame</returns>
         public int GetPressedLetterCount(KeyboardState keyboardState)
         {
+            // gathers every key the player pressed
             Keys[] pressedKeys = keyboardState.GetPressedKeys();
             int counter = 0;
 
+            // goes through every key the player pressed
             for(int i = 0; i < pressedKeys.Length; i++)
             {
+                // if its a letter, incrememnt the count by one
                 if(letterKeys.Contains(pressedKeys[i]))
                     counter++;
             }
 
+            // return the count
             return counter;
         }
 
+        /// <summary>
+        /// takes in a character and returns the key
+        /// associated with it.
+        /// </summary>
+        /// <param name="character">a character</param>
+        /// <returns>the key associated with the character</returns>
         public Keys GetKeyFromChar(String character)
         {
             character = character.ToLower();
@@ -295,30 +364,47 @@ namespace Multitasking
                 return Keys.None;
         }
 
+        /// <summary>
+        /// types a character in the typing simulator
+        /// </summary>
         public void TypeChar()
         {
+            // checks if we are actually within a prompt
             if(currentLineIndex != -1)
             {
+                // if we are, increments the character positon by one and moves to the next typeable character
                 currentCharIndex++;
                 MoveToNextAvailableChar();
             }
         }
 
+        /// <summary>
+        /// moves the current character to the next one
+        /// that the player is allowed to type. in practice
+        /// this means ignoring spaces, punctuation, and 
+        /// new lines.
+        /// </summary>
         public void MoveToNextAvailableChar()
         {
+            // checks if we are actually within a prompt
             if(currentLineIndex != -1)
             {
+                // if we are, checks if that was the last character
                 if(currentCharIndex == CurrentPrompt[currentLineIndex].Length)
                 {
+                    // if it was, move to the next line
                     MoveToNextLine();
                 }
 
+                // checks if we are now at the end of the prompt
                 if(currentLineIndex != -1)
                 {
+                    // if we are, skips characters until we arrive at a typeable one
                     while(skippedCharacters.Contains(CurrentPrompt[currentLineIndex][currentCharIndex].ToString()))
                     {
                         currentCharIndex++;
 
+                        // if we hit the end of a line, moves to the next line
                         if(currentCharIndex == CurrentPrompt[currentLineIndex].Length)
                         {
                             MoveToNextLine();
@@ -328,30 +414,42 @@ namespace Multitasking
             }
         }
 
+        /// <summary>
+        /// moves the current character to a new line
+        /// and resets all variables accordingly.
+        /// </summary>
         public void MoveToNextLine()
         {
+            // checks to make sure we are still in the prompt
             if(currentLineIndex != -1)
             {
+                // if we are, reset the char index
                 currentCharIndex = 0;
 
+                // if this is the end of the prompt, update the line index accordingly
                 if(currentLineIndex == letterPrompt.Count - 1)
                 {
                     currentLineIndex = -1;
                 }
+                // otherwise just increment the line index by one and move to the next typeable character
                 else
                 {
                     currentLineIndex++;
                     MoveToNextAvailableChar();
                 }
 
+                // if we are not at the end of the prompt
                 if(currentLineIndex != -1)
                 {
+                    // skips passed any empty lines
                     while(letterPrompt[currentLineIndex] == "")
                     {
+                        // if we hit the end of the prompt, update the line index accordingly
                         if(currentLineIndex == letterPrompt.Count)
                         {
                             currentLineIndex = -1;
                         }
+                        // otherwise, just increment the line index
                         else
                         {
                             currentLineIndex++;
